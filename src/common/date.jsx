96717e7ca8ca5bc0bf2bc1e47d6008558e7f40ce
rayaadinda <rayaadinda78@gmail.com>
2024-10-01 19:55:13 +0700

    add real time date

--- src/common/date.jsx
+++ src/common/date.jsx
@@ -13,18 +13,22 @@ let months = [
 	"Dec",
 ]
 
-let days = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-]
-
 export const getDay = (timestamp) => {
-	let date = new Date(timestamp)
+	const now = new Date()
+	const date = new Date(timestamp)
+	const diff = now.getTime() - date.getTime()
+	const seconds = Math.floor(diff / 1000)
+	const minutes = Math.floor(seconds / 60)
+	const hours = Math.floor(minutes / 60)
+	const days = Math.floor(hours / 24)
 
-	return `${date.getDate()} ${months[date.getMonth()]}`
+	if (days > 0) {
+		return `${date.getDate()} ${months[date.getMonth()]}`
+	} else if (hours > 0) {
+		return `${hours}h`
+	} else if (minutes > 0) {
+		return `${minutes}m`
+	} else {
+		return `${seconds}s`
+	}
 }
