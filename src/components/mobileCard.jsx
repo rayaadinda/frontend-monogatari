a02e66a1d60239a30abee90ae8b886bfef940002
rayaadinda <rayaadinda78@gmail.com>
2024-10-01 19:56:11 +0700

    change date placement

--- src/components/mobileCard.jsx
+++ src/components/mobileCard.jsx
@@ -33,19 +33,21 @@ const MobileCard = ({ content, author }) => {
 					className="w-8 h-8 rounded-full mr-2"
 				/>
 				<div>
-					<p className="text-sm font-semibold">@{username}</p>
+					<p className="text-xl font-semibold mr-4">
+						@{username}{" "}
+						<span className="text-base font-normal">{getDay(publishedAt)}</span>
+					</p>
 					<p className="text-sm text-gray-500">in Jakarta</p>
 				</div>
 			</div>
 			<h2 className="font-gelasio font-medium mb-2">{title}</h2>
 			<p className="text-sm text-gray-700 mb-2">{des}</p>
-			<img src={banner} className=" rounded-lg mb-2" />
+			<img src={banner} className="aspect-video rounded-lg mb-2" />
 			<div className="flex items-center text-xs text-gray-500">
-				<span className="mr-2">{getDay(publishedAt)}</span>
 				<span className="flex items-center gap-2">
 					<i className="fi fi-rr-heart text-lg"></i>
 					{total_likes}
-					<span className=" text-lg py-1 px-4">#{tags[0]} </span>
+					<span className=" text-lg py-1 px-4">{tags[0]} </span>
 				</span>
 			</div>
 		</motion.div>
